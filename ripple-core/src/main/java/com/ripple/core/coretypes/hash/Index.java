117bcb9c23adc7b5a41429534c556cd2facbb22a
Nicholas Dudfield <ndudfield@gmail.com>
2014-08-26 12:25:35 +0700

    Index: Add ledgerHashes methods

--- ripple-core/src/main/java/com/ripple/core/coretypes/hash/Index.java
+++ ripple-core/src/main/java/com/ripple/core/coretypes/hash/Index.java
@@ -5,6 +5,7 @@ import com.ripple.core.coretypes.Currency;
 import com.ripple.core.coretypes.Issue;
 import com.ripple.core.coretypes.hash.prefixes.HashPrefix;
 import com.ripple.core.coretypes.hash.prefixes.LedgerSpace;
+import com.ripple.core.coretypes.uint.UInt32;
 import com.ripple.core.coretypes.uint.UInt64;
 import com.ripple.core.serialized.SerializedType;
 
@@ -52,6 +53,10 @@ public class Index {
     }
 
     public static Hash256 directoryNode(Hash256 base, UInt64 nodeIndex) {
+        if (nodeIndex == null || nodeIndex.isZero()) {
+            return base;
+        }
+
         HalfSha512 hash = prefixed256(LedgerSpace.dirNode);
 
         for (SerializedType component : new SerializedType[]{base, nodeIndex})
@@ -60,7 +65,7 @@ public class Index {
         return hash.finish();
     }
 
-    public static Hash256 accountID(AccountID accountID) {
+    public static Hash256 accountRoot(AccountID accountID) {
         HalfSha512 hash = prefixed256(LedgerSpace.account);
         accountID.toBytesSink(hash);
         return hash.finish();
@@ -85,5 +90,14 @@ public class Index {
     public static Hash256 bookEnd(Hash256 base) {
         byte[] end = base.bigInteger().add(Hash256.bookBaseSize).toByteArray();
         return new Hash256(end);
+    }
+
+    public static Hash256 ledgerHashes(long prev) {
+        return prefixed256(LedgerSpace.skipList)
+                    .add(new UInt32(prev >> 16))
+                    .finish();
+    }
+    public static Hash256 ledgerHashes() {
+        return prefixed256(LedgerSpace.skipList).finish();
     }
 }
