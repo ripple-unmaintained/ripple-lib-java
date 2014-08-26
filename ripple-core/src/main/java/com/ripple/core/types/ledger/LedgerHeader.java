94c0b48181ce6d63e0f1b92d3f74cfeee3d50576
Nicholas Dudfield <ndudfield@gmail.com>
2014-08-26 12:31:56 +0700

    LedgerHeader: Make members public

--- ripple-core/src/main/java/com/ripple/core/types/ledger/LedgerHeader.java
+++ ripple-core/src/main/java/com/ripple/core/types/ledger/LedgerHeader.java
@@ -1,6 +1,6 @@
 package com.ripple.core.types.ledger;
 
-import com.ripple.core.binary.BinaryReader;
+import com.ripple.core.binary.STReader;
 import com.ripple.core.coretypes.RippleDate;
 import com.ripple.core.serialized.BinaryParser;
 import com.ripple.core.coretypes.hash.Hash256;
@@ -11,23 +11,23 @@ import com.ripple.core.coretypes.uint.UInt8;
 import java.util.Date;
 
 public class LedgerHeader {
-    UInt32  version;         // Always 0x4C475200 (LWR) (Secures signed objects)
-    UInt32  sequence;        // Ledger Sequence (0 for genesis ledger)
-    UInt64  totalXRP;        //
-    Hash256 previousLedger;  // The hash of the previous ledger (0 for genesis ledger)
-    Hash256 transactionHash; // The hash of the transaction tree's root node.
-    Hash256 stateHash;       // The hash of the state tree's root node.
-    UInt32  parentCloseTime; // The time the previous ledger closed
-    UInt32  closeTime;       // UTC minute ledger closed encoded as seconds since 1/1/2000 (or 0 for genesis ledger)
-    UInt8   closeResolution; // The resolution (in seconds) of the close time
-    UInt8   closeFlags;      // Flags
+    public UInt32  version;         // Always 0x4C475200 (LWR) (Secures signed objects)
+    public UInt32  sequence;        // Ledger Sequence (0 for genesis ledger)
+    public UInt64  totalXRP;        //
+    public Hash256 previousLedger;  // The hash of the previous ledger (0 for genesis ledger)
+    public Hash256 transactionHash; // The hash of the transaction tree's root node.
+    public Hash256 stateHash;       // The hash of the state tree's root node.
+    public UInt32  parentCloseTime; // The time the previous ledger closed
+    public UInt32  closeTime;       // UTC minute ledger closed encoded as seconds since 1/1/2000 (or 0 for genesis ledger)
+    public UInt8   closeResolution; // The resolution (in seconds) of the close time
+    public UInt8   closeFlags;      // Flags
 
-    Date closeDate;
+    public Date closeDate;
 
     public static LedgerHeader fromParser(BinaryParser parser) {
-        return fromReader(new BinaryReader(parser));
+        return fromReader(new STReader(parser));
     }
-    public static LedgerHeader fromReader(BinaryReader reader) {
+    public static LedgerHeader fromReader(STReader reader) {
         LedgerHeader ledger = new LedgerHeader();
 
         ledger.version = reader.uInt32();
