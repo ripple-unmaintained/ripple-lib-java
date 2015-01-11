9e124420aa62c40088f55a8f20453477ec88cf87
Nicholas Dudfield <ndudfield@gmail.com>
2015-01-11 14:43:38 +0700

    [WIP] Fix test

--- ripple-core/src/main/java/com/ripple/core/coretypes/hash/prefixes/HashPrefix.java
+++ ripple-core/src/main/java/com/ripple/core/coretypes/hash/prefixes/HashPrefix.java
@@ -3,21 +3,21 @@ package com.ripple.core.coretypes.hash.prefixes;
 import com.ripple.core.coretypes.uint.UInt32;
 
 public enum HashPrefix implements Prefix {
-    transactionID(0x54584E00),
+    transactionID(0x54584E00L),
     // transaction plus metadata
-    txNode(0x534E4400),
+    txNode(0x534E4400L),
     // account state
-    leafNode(0x4D4C4E00),
+    leafNode(0x4D4C4E00L),
     // inner node in tree
-    innerNode(0x4D494E00),
+    innerNode(0x4D494E00L),
     // ledger master data for signing
-    ledgerMaster(0x4C575200),
+    ledgerMaster(0x4C575200L),
     // inner transaction to sign
-    txSign(0x53545800),
+    txSign(0x53545800L),
     // validation for signing
-    validation(0x56414C00),
+    validation(0x56414C00L),
     // proposal for signing
-    proposal(0x50525000);
+    proposal(0x50525000L);
 
     public UInt32 uInt32;
     public byte[] bytes;
