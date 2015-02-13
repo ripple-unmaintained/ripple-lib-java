f41f4cfe524eabfee96bfae528fa6a15f83f2737
Nicholas Dudfield <ndudfield@gmail.com>
2015-02-13 16:25:25 +0700

    Update RippleState: * Add issuedByHigh * Add issuedByLow * Add issuedTo * Remove (broken) balanceFor * Add comment

--- ripple-core/src/main/java/com/ripple/core/types/known/sle/entries/RippleState.java
+++ ripple-core/src/main/java/com/ripple/core/types/known/sle/entries/RippleState.java
@@ -1,23 +1,85 @@
 package com.ripple.core.types.known.sle.entries;
 
-import com.ripple.core.coretypes.AccountID;
-import com.ripple.core.coretypes.Amount;
-import com.ripple.core.coretypes.Currency;
-import com.ripple.core.coretypes.Issue;
+import com.ripple.core.coretypes.*;
 import com.ripple.core.coretypes.hash.Hash256;
 import com.ripple.core.coretypes.hash.Index;
 import com.ripple.core.coretypes.uint.UInt32;
 import com.ripple.core.coretypes.uint.UInt64;
 import com.ripple.core.enums.LedgerFlag;
-import com.ripple.core.serialized.enums.LedgerEntryType;
 import com.ripple.core.fields.Field;
 import com.ripple.core.fields.TypedFields;
+import com.ripple.core.serialized.enums.LedgerEntryType;
 import com.ripple.core.types.known.sle.ThreadedLedgerEntry;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class RippleState extends ThreadedLedgerEntry {
+    /*
+    The RippleState is a ledger entry which roughly speaking defines the balance and
+    trust limits between two accounts.
+
+    Like all current ledger entries, it has a canonical form for hashing that
+    doesn't necessarily communicate the information in a way that is clear or
+    obvious to a human. The current json format for some ledger entries is very
+    close to the hashing format, and isn't any better.
+
+    The two accounts on the link are categorized into a low account, and a high
+    account by comparing the 160bits of their account id as a big endian unsigned
+    integer.
+
+    There is one and only one `Balance` stored, using an amount struct with a
+    neutral `issuer`. ( A uint160 with the numerical value of `1` was chosen as a
+    placeholder, as the canonical way to represent a null account )
+
+    The Balance can be negative, zero, or positive and is in terms of the Low
+    account, such that when it's positive, it defines how much credit the High
+    account has issued.
+
+    Between any account, there can be two types of balance changes, issuance and
+    redemption. A redemption is the transferal of previously issued IOUs back to the
+    owner.
+
+    This implies that for any account, that can they can make a transferal using
+    funds from two distinct balances. After making this distinction it becomes
+    nonsensical to say that an account holds negative IOUs from the opposite line.
+
+    Thus, the one Balance in a ripple state actually implies 4 distinct balances.
+
+    ```
+    {
+     ...
+     "Balance" : {
+        "currency" : "USD",
+        "issuer" : "rrrrrrrrrrrrrrrrrrrrBZbvji",
+        "value" : "100"
+     },
+     "HighLimit" : {
+        "currency" : "USD",
+        "issuer" : "rPMh7Pi9ct699iZUTWaytJUoHcJ7cgyziK",
+        "value" : "500"
+     },
+     "LowLimit" : {
+        "currency" : "USD",
+        "issuer" : "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh",
+        "value" : "500"
+     },
+    }
+    ```
+
+    lowAccount = LowLimit.issuer
+    hiAccount = HighLimit.issuer
+
+    Balances:
+        lowAccount has 100/USD/highAccount
+        lowAccount has 0/USD/lowAccount on the line
+
+        highAccount has 0/USD/lowAccount
+        highAccount has -100/USD/highAccount on the line
+
+
+    * */
+
     public RippleState() {
         super(LedgerEntryType.RippleState);
     }
@@ -82,28 +144,30 @@ public class RippleState extends ThreadedLedgerEntry {
         return balance().currency();
     }
 
-    public Amount balanceFor(AccountID owner) {
-        TypedFields.AmountField field = limitFieldFor(owner);
-        Amount balance = balance();
-        AccountID issuer = lowAccount();
-        if (field == Amount.HighLimit) {
-            balance = balance.negate();
-            issuer = highAccount();
+    private Amount issuedBy(boolean hi) {
+        Amount balance;
+
+        if (hi) {
+            balance = balance().newIssuer(highAccount());
+        } else {
+            balance = balance().negate().newIssuer(lowAccount());
         }
-        return balance.newIssuer(issuer);
+
+        if (!balance.isPositive()) {
+            balance = balance.issue().amount(0);
+        }
+        return balance;
     }
 
-    public Amount issued() {
-        Amount balance = balance();
-        if (balance.isNegative()) {
-            // Balance is in terms of the LowAccount, so if the
-            // balance is negative, that means it has issued
-            return balance.negate().newIssuer(lowAccount());
-        } else {
-            // If it's positive, then the LowAccount has money
-            // issued by the highAccount
-            return balance.newIssuer(highAccount());
-        }
+    public Amount issuedByHigh() {
+        return issuedBy(true);
+    }
+    public Amount issuedByLow() {
+        return issuedBy(false);
+    }
+
+    public Amount issuedTo(AccountID accountID) {
+        return issuedBy(isLowAccount(accountID));
     }
 
     @Deprecated() // "not deprecated but needs fixing"
