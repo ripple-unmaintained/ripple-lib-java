547b8129a6d95031ee52a0f68acd96a3fef82589
Nicholas Dudfield <ndudfield@gmail.com>
2015-02-15 17:46:29 +0700

    Add constructor to SignedTransaction

--- ripple-core/src/main/java/com/ripple/core/types/known/tx/signed/SignedTransaction.java
+++ ripple-core/src/main/java/com/ripple/core/types/known/tx/signed/SignedTransaction.java
@@ -13,13 +13,24 @@ import com.ripple.core.types.known.tx.Transaction;
 import com.ripple.crypto.ecdsa.IKeyPair;
 
 public class SignedTransaction {
+    public SignedTransaction(Transaction txn) {
+        this.txn = txn;
+    }
+
+    @Deprecated
+    public SignedTransaction() {}
+
     public Transaction txn;
     public Hash256 hash;
     public Hash256 signingHash;
     public Hash256 previousSigningHash;
     public String tx_blob;
 
-    public void prepare(IKeyPair keyPair, Amount fee, UInt32 Sequence, UInt32 lastLedgerSequence) {
+    public void prepare(IKeyPair keyPair,
+                        Amount fee,
+                        UInt32 Sequence,
+                        UInt32 lastLedgerSequence) {
+
         VariableLength pubKey = new VariableLength(keyPair.pubBytes());
 
         // This won't always be specified
@@ -32,6 +43,7 @@ public class SignedTransaction {
         if (fee != null) {
             txn.put(Amount.Fee, fee);
         }
+
         txn.put(VariableLength.SigningPubKey, pubKey);
 
         if (Transaction.CANONICAL_FLAG_DEPLOYED) {
@@ -57,7 +69,7 @@ public class SignedTransaction {
             previousSigningHash = null;
             throw new RuntimeException(e);
         } /*else {*/
-        previousSigningHash = signingHash;
+            previousSigningHash = signingHash;
         // }
     }
 
