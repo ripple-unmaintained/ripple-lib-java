ac9d8e1225047ac95031b50699f348bd1aee5fed
Nicholas Dudfield <ndudfield@gmail.com>
2016-04-19 16:17:14 +0700

    Add IssuePair#toJSON

--- ripple-core/src/main/java/com/ripple/core/coretypes/IssuePair.java
+++ ripple-core/src/main/java/com/ripple/core/coretypes/IssuePair.java
@@ -1,5 +1,7 @@
 package com.ripple.core.coretypes;
 
+import org.json.JSONObject;
+
 import java.text.MessageFormat;
 
 public class IssuePair implements Comparable<IssuePair> {
@@ -23,5 +25,12 @@ public class IssuePair implements Comparable<IssuePair> {
             cmp = gets.compareTo(o.gets);
         }
         return cmp;
+    }
+
+    public JSONObject toJSON() {
+        JSONObject ret = new JSONObject();
+        ret.put("taker_gets", gets.toJSON());
+        ret.put("taker_pays", pays.toJSON());
+        return ret;
     }
 }
