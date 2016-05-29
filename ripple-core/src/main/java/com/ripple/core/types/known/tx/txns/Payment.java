09526b338bc65721f9dbc541187f59dec06c5418
Roberto Santacroce Martins <miles.chet@gmail.com>
2016-05-29 02:10:20 -0300

    Added DeliverMin to Payment

--- ripple-core/src/main/java/com/ripple/core/types/known/tx/txns/Payment.java
+++ ripple-core/src/main/java/com/ripple/core/types/known/tx/txns/Payment.java
@@ -20,11 +20,13 @@ public class Payment extends Transaction {
     public Amount sendMax() {return get(Amount.SendMax);}
     public AccountID destination() {return get(AccountID.Destination);}
     public PathSet paths() {return get(PathSet.Paths);}
+    public Amount deliverMin() { return get(Amount.Amount);}
     public void destinationTag(UInt32 val) {put(Field.DestinationTag, val);}
     public void invoiceID(Hash256 val) {put(Field.InvoiceID, val);}
     public void amount(Amount val) {put(Field.Amount, val);}
     public void sendMax(Amount val) {put(Field.SendMax, val);}
     public void destination(AccountID val) {put(Field.Destination, val);}
     public void paths(PathSet val) {put(Field.Paths, val);}
-
+    public void deliverMin(Amount val) { put(Field.DeliverMin, val);}
+    
 }
